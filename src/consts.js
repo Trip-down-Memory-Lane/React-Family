119456bc5bac89cc30a0743d951c7387031c1f82
ralitsaDmt <redelcheva@gmail.com>
2016-11-29 13:31:10 +0200

    Added credentials for Kinvey.

---
+++ src/consts.js
@@ -0,0 +1,12 @@
+let consts = (function () {
+        const baseUrl = "https://baas.kinvey.com/";
+        const appKey = "kid_SkHaVTqGx";
+        const appSecret = "53990916d9bf469798b755ec323fd517";
+        const kinveyAppAuthHeaders = {
+            'Authorization': "Basic " + btoa(appKey + ":" + appSecret),
+        };
+
+        return {baseUrl, appKey, appSecret, kinveyAppAuthHeaders};
+})();
+
+export default consts;
