fa99df6ec20a361846652bd38e8f09d2c19e8405
ralitsaDmt <redelcheva@gmail.com>
2016-11-29 13:32:12 +0200

    Added login function and function for saving credentials after login/register

---
+++ src/controllers/UserController.js
@@ -0,0 +1,25 @@
+import KinveyRequester from '.././KinveyRequester';
+
+    function login(username, password) {
+        KinveyRequester.loginUser(username, password)
+            .then(loginSuccess.bind(this));
+
+        function loginSuccess(userInfo) {
+            saveAuthInSession(userInfo);
+            console.log(userInfo.username + ' successfully logged in.');
+        }
+    }
+
+    function saveAuthInSession(userInfo) {
+        sessionStorage.setItem('authToken', userInfo._kmd.authtoken);
+        sessionStorage.setItem('userId', userInfo._id);
+        sessionStorage.setItem('username', userInfo.username);
+
+        // this.setState({
+        //     username: userInfo.username,
+        //     userId: userInfo._id
+        // });
+    }
+
+
+export default login;
