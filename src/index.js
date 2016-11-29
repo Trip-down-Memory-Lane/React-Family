66b9f4c27e90fb371b53ee9c2ecc42573b2efaea
PetyoPlPetrov <petyoplpetrov@gmail.com>
2016-11-29 17:46:34 +0200

    createRootTest funcion

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import './index.css';
 import Path from '../src/constants/constant'
 import changeView from './controllers/ViewManager';
 
-
-
-changeView(Path.registerView());
+changeView(Path.familyTreeView());
