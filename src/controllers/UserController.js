7c707a1ab4ee5b55e9a8505303f9b6da2aa03cb2
ralitsaDmt <redelcheva@gmail.com>
2016-11-29 23:53:00 +0200

    Added register funcion. Function saveAuth taken out so it can be reused.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,24 +1,45 @@
 import KinveyRequester from '../services/KinveyRequester';
 
-class UserController{
+class UserController {
 
-     login(username,password){
+    login(username, password) {
         KinveyRequester.loginUser(username, password)
             .then(loginSuccess.bind(this));
 
         function loginSuccess(userInfo) {
-            saveAuthInSession(userInfo);
+            UserController.saveAuthInSession(userInfo);
+            // TODO render profile page view
             console.log(userInfo.username + ' successfully logged in.');
         }
+    }
 
-        function saveAuthInSession(userInfo) {
-            sessionStorage.setItem('authToken', userInfo._kmd.authtoken);
-            sessionStorage.setItem('userId', userInfo._id);
-            sessionStorage.setItem('username', userInfo.username);
-
+    register(username, password, confirmPassword) {
+        if (password !== confirmPassword) {
+            // TODO error message
+            alert("Passwords don't match.");
         }
+        // else will be removed after rendering the error message
+        else {
+            KinveyRequester.registerUser(username, password)
+                .then(registerSuccess.bind(this));
+                // TODO catch error message
+
+
+            function registerSuccess(userInfo) {
+                UserController.saveAuthInSession(userInfo);
+
+                // TODO render edit profile page view
+                console.log(userInfo.username + ' successfully registered.');
+            }
+        }
+    }
+
+    static saveAuthInSession(userInfo) {
+        sessionStorage.setItem('authToken', userInfo._kmd.authtoken);
+        sessionStorage.setItem('userId', userInfo._id);
+        sessionStorage.setItem('username', userInfo.username);
     }
 }
 
-let userController=new UserController();
+let userController = new UserController();
 export default  userController;
