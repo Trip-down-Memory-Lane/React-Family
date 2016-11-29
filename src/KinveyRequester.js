5d551f7ba8c59729654fae07359726b359dd0c60
ralitsaDmt <redelcheva@gmail.com>
2016-11-29 13:30:26 +0200

    Added login, register and logout requests to Kinvey.

---
+++ src/KinveyRequester.js
@@ -0,0 +1,41 @@
+import $ from 'jquery';
+import consts from './consts';
+
+const KinveyRequester = (function(){
+
+    function loginUser(username, password) {
+        return $.ajax({
+                method: "POST",
+                url: consts.baseUrl + "user/" + consts.appKey + "/login",
+                headers: consts.kinveyAppAuthHeaders,
+                data: { username, password }
+            });
+    }
+
+    function registerUser(username, password) {
+        return $.ajax({
+            method: "POST",
+            url: consts.baseUrl + "user/" + consts.appKey + "/",
+            headers: consts.kinveyAppAuthHeaders,
+            data: { username, password }
+        });
+    }
+
+    function getKinveyUserAuthHeaders() {
+        return {
+            'Authorization': "Kinvey " + sessionStorage.getItem('authToken'),
+        };
+    }
+
+    function logoutUser() {
+        return $.ajax({
+            method: "POST",
+            url: consts.baseUrl + "user/" + consts.appKey + "/_logout",
+            headers: getKinveyUserAuthHeaders(),
+        });
+    }
+
+    return { loginUser, registerUser, logoutUser }
+})();
+
+export default KinveyRequester;
