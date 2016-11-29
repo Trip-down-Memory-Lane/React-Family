ce5c371f252f74174d34b78275cf16884f4b6a53
ralitsaDmt <redelcheva@gmail.com>
2016-11-29 13:33:03 +0200

    Added login binding on view change

--- src/controllers/viewManager.js
+++ src/controllers/viewManager.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
-import $ from 'jquery'
-import ReactDOM from 'react-dom';
-import Login from '../views/LoginView'
-import Register from '../views/RegisterView'
-
+ import React, {Component} from 'react';
+ import $ from 'jquery'
+ import ReactDOM from 'react-dom';
+ import LoginView from '../views/LoginView'
+ import Register from '../views/RegisterView'
+ import login from './UserController';
 
 function changeView(view, id) {
     let component;
     switch(view) {
-        case`login`: component = <Login/>; break;
+        case`login`: component = <LoginView onsubmit={login.bind(this)}/>; break;
         case`register`: component = <Register/>; break;
 
         default: break;
