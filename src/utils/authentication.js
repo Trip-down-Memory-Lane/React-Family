fb4085673a0b5547f409b38e4141bd6b0e9577c0
ralitsaDmt <redelcheva@gmail.com>
2016-12-01 00:35:55 +0200

    added function isAuthenticated and moved kinvey user authentication functon from kinveyRequester

---
+++ src/utils/authentication.js
@@ -0,0 +1,17 @@
+import Path from '../constants/constant';
+
+class Authenticator{
+
+    isAuthenticated(){
+        return sessionStorage.getItem('userId') ;
+    }
+
+    getKinveyUserAuthHeaders() {
+    return {
+        'Authorization': Path.database() + sessionStorage.getItem('authToken'),
+    };
+}
+}
+
+let authenticator = new Authenticator();
+export default authenticator
