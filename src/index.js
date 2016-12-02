c6ae4ddcc76cee0f5bf9f3561dd4c5f06a6908c8
achobanov <alexchobanov@gmail.com>
2016-12-02 21:25:46 +0200

    fixed bug

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import './index.css';
 import ViewManager from './controllers/ViewManager';
 
-import React from "react";
-import ReactDOM from "react-dom";
-import FamilyTreeView from "./views/FamilyTreeView";
+// DEV ONLY
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import FamilyTreeView from "./views/FamilyTreeView";
+//
+// ReactDOM.render(
+//     <FamilyTreeView />,
+//     document.getElementById(`root`)
+// );
 
-ReactDOM.render(
-    <FamilyTreeView />,
-    document.getElementById(`root`)
-);
 
-
-// ViewManager.changeView();
+ViewManager.changeView();
 
